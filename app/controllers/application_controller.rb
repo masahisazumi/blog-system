2158ea7f72f8eb69dd73c33783a27a0d5b6dd275
masahisazumi <m.hisazumi@gmail.com>
2019-08-30 16:52:08 +0900

    cofigure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :cofigure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def cofigure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys[:idname])
+    devise_parameter_sanitizer.permit(:account_update, keys[:idname])
+  end
 end
