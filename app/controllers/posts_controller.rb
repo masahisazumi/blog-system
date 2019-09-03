bbc6bb9942085ebc865bd00327c65c1fe9293449
masahisazumi <m.hisazumi@gmail.com>
2019-09-03 17:40:39 +0900

    authenticate_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @posts = current_user.posts
